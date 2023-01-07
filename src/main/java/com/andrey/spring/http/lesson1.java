fe4ee07da0ed6caad91dd9a70ba576c82ad4e549
Andrey Shigartsov <a.shchigartsov@yandex.ru>
2023-01-08 03:09:49 +0700

    Git second

--- src/main/java/com/andrey/spring/http/lesson1.java
+++ src/main/java/com/andrey/spring/http/lesson1.java
@@ -2,6 +2,6 @@ package com.andrey.spring.http;
 
 public class lesson1 {
     private static void task1(){
-        System.out.println("Git first");
+        System.out.println("Git second");
     }
 }
